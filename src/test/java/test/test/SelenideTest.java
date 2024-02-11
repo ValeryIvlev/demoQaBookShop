53dea24de9e259c9d562bcdfb7095e61bed2a22d
Valery <qaivlev@yandex.ru>
2024-02-11 17:16:19 +0300

    13

--- src/test/java/test/test/SelenideTest.java
+++ src/test/java/test/test/SelenideTest.java
@@ -1,11 +1,8 @@
 package test.test;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Cookie;
 import test.test.data.TestData;
 import test.test.helpers.WithLogin;
 import test.test.models.user.ApiUserResponse;
@@ -13,15 +10,6 @@ import test.test.pages.LoginPage;
 import test.test.pages.ProfilePage;
 import test.test.steps.OrderSteps;
 import test.test.steps.RegistrationSteps;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static test.test.data.CookiesData.EXPIRES;
-import static test.test.data.CookiesData.TOKEN;
-import static test.test.data.UserData.USER_ID;
-import static test.test.data.UserData.USER_NAME;
 
 
 public class SelenideTest extends TestBase{
